7b9d88544e23525588025a129c569e11b5f763e6
Doug Stanley <DouglasStanley@outlook.com>
2022-04-24 20:47:51 -0700

    More Tweeks with Serilog and KeyC

--- microsvc-weather/Controllers/WeatherForecastController.cs
+++ microsvc-weather/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace microsvc_weather.Controllers
 {
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
@@ -11,16 +13,18 @@ namespace microsvc_weather.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _lgr;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            _lgr = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _lgr.LogDebug($"You are authenticated as : {User.FindFirst("preferred_username")?.Value}");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
--- microsvc-weather/Program.cs
+++ microsvc-weather/Program.cs
@@ -1,35 +1,92 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Events;
+using System.Security.Claims;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Debug()
+                    .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)  // Throttles EF Logging
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                    .WriteTo.Console()
+                    .CreateLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Host.UseSerilog((ctx, lc) => lc
-            .MinimumLevel.Debug()
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)  // TODO: Integrate with Config File
-            .Enrich.FromLogContext()
-            .WriteTo.Console()
-            .WriteTo.Seq("http://nexlog:5109/"));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddTransient<IClaimsTransformation, ClaimsTransformer>();
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+    }).AddJwtBearer(o =>
+    {
+
+        o.Authority = builder.Configuration["Jwt:Authority"];
+        o.Audience = builder.Configuration["Jwt:Audience"];
+    //o.Authority = Configuration["Jwt:Authority"];
+    //o.Audience = Configuration["Jwt:Audience"];
+    o.RequireHttpsMetadata = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            RoleClaimType = ClaimTypes.Role
+        };
+
+        o.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = c =>
+            {
+                Log.Error(c.Exception, "XXX Auth Failed");
+                c.NoResult();
+                c.Response.StatusCode = 500;
+                c.Response.ContentType = "text/plain";
+
+                if (builder.Environment.IsDevelopment())
+                {
+                    return c.Response.WriteAsync(c.Exception.ToString());
+                }
+
+                return c.Response.WriteAsync("An error occured processing your authentication.");
+            }
+        };
+    });
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)  // TODO: Integrate with Config File
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Seq("http://nexlog:5109/"));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    // app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "App Failed to Start");
+}
